624a4435a8d71e0aef06fdbef8367a454efc5a0c
TomasRullan <tomasrullan@yahoo.com>
2024-02-18 19:09:50 -0500

    changed index html

--- assets/javascipt.js
+++ assets/javascipt.js
@@ -1,46 +1,44 @@
-var saveButton = $("button");
-var dayToday = moment().format("dddd, MMMM Do");
-var storedHourInput = JSON.parse(localStorage.getItem("taskPerHour"));
-var containerTimeBlocks = $("#container");
-var hourRows = $(".hour");
-
-//lists the current day and date in the Jumbotron.
-$("#currentDay").text(dayToday);
-
-var currentHour = moment().format("HH");
-
-//this checks the current hour and apples the relevent CSS class to that time block.
-for (var i = 0; i < hourRows.length; i++) {
-	var hourRow = $(hourRows[i]);
-	var hourRowData = hourRow.data("hour");
-	if (currentHour > hourRowData) {
-		hourRow.next().addClass("past");
-	} else if (currentHour < hourRowData) {
-		hourRow.next().addClass("future");
-	} else {
-		hourRow.next().addClass("present");
-	}
-}
-
-if (storedHourInput === null) {
-	storedHourInput = Array(9).fill("");
-}
-
-//this loops through each input of the html elements and places in the locally stored values.
-for (var i = 0; i < storedHourInput.length; i++) {
-	containerTimeBlocks
-		.children()
-		.children("input")
-		.eq(i)
-		.val(storedHourInput[i]);
-}
-
-//when this button is clicked it saves to local storage by selecting the previous sibling.
-saveButton.on("click", function (event) {
-	var currentTarget = $(event.currentTarget);
-	var calendarHourInput = currentTarget.prev();
-	var buttonIndex = currentTarget.data("buttonindex");
-	storedHourInput[buttonIndex] = calendarHourInput.val();
-	//saves to local storage ->
-	localStorage.setItem("taskPerHour", JSON.stringify(storedHourInput));
-});
+$(function () {
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+  
+    $(".time-block").each(function () {
+      var currentHour = parseInt(dayjs().format("h"));
+      var currentPeriod = dayjs().format("A");
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      var blockPeriod = $(this).attr("id").split("-")[2];
+      console.log(currentHour, blockHour, blockPeriod);
+  
+      if (currentPeriod === blockPeriod) {
+        if (blockHour < currentHour) {
+          $(this).addClass("bg-dark-subtle");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("bg-dark-subtle");
+          $(this).addClass("bg-light");
+        } else {
+          $(this).removeClass("bg-dark-subtle");
+          $(this).removeClass("bg-light");
+          $(this).addClass("bg-primary-subtle");
+        }
+      } else if (currentPeriod === "AM" && blockPeriod === "PM") {
+        $(this).addClass("bg-primary-subtle");
+      } else if (currentPeriod === "PM" && blockPeriod === "AM") {
+        $(this).addClass("bg-dark-subtle");
+      }
+    });
+  
+    $(".saveBtn").on("click", function () {
+      var hourId = $(this).parent().attr("id");
+      var input = $(this).siblings(".description").val();
+  
+      localStorage.setItem(hourId, input);
+    });
+  
+    $(".description").each(function () {
+      var id = $(this).parent().attr("id");
+      var saved = localStorage.getItem(id);
+  
+      if (saved !== null) {
+        $(this).val(saved);
+      }
+    });
+  });
